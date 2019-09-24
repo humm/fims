fc3351b6edc384fcea00a839a7e8edac4de57359
dell-company <10806623@qq.com>
2019-09-24 13:58:55 +0800

    版本信息调整

--- src/main/resources/database/other.sql
+++ src/main/resources/database/other.sql
@@ -2,7 +2,6 @@ call drop_table('sys_version');
 create table sys_version
 (
     version_id      number(30) primary key,
-    version_no      varchar2(50)  not null,
     version_type    varchar2(50)  not null,
     version_content varchar2(500) not null,
     version_date    date,
@@ -10,8 +9,6 @@ create table sys_version
 );
 comment on column sys_version.version_id
     is '修订id';
-comment on column sys_version.version_no
-    is '修订版本';
 comment on column sys_version.version_type
     is '修订类型';
 comment on column sys_version.version_content
